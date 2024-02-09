1bdac819f6f7dd0f5133020cf3c196e4080a8e30
Hardik Parmar <121241956+hardikjp7@users.noreply.github.com>
2024-02-09 15:18:12 +0530

    Update health.py

--- health.py
+++ health.py
@@ -1,15 +1,18 @@
-# ### Health Management APP
-# from dotenv import load_dotenv
-
-# load_dotenv() ## load all the environment variables
-
 import streamlit as st
-import os
 import google.generativeai as genai
 from PIL import Image
 
 # Set page configuration
 st.set_page_config(page_title="Gemini Health App")
+
+# Sidebar guide
+st.sidebar.markdown("""
+### Guide
+1. Enter your Gemini API key in the provided input field.
+2. The default prompt is already integrated into the application.
+3. Simply upload a photo of food to receive calorie information.
+4. For additional details or a custom prompt, utilize the "Provide Prompt" input to access extra information via the Gemini Vision Pro model.
+""")
 
 # Configure Google Gemini Pro Vision API with the API key from the input field
 api_key = st.sidebar.text_input("Enter your Google API Key:", key="api_key")
@@ -41,8 +44,6 @@ def input_image_setup(uploaded_file):
         raise FileNotFoundError("No file uploaded")
     
 ##initialize our streamlit app
-# st.header("Gemini Health App")
-
 st.title("Gemini Health App")
 
 input_text = st.text_input("Input Prompt: ", key="input")
@@ -56,13 +57,13 @@ submit = st.button("Tell me the total calories")
 
 input_prompt="""
 You are an expert in nutritionist where you need to see the food items from the image
-               and calculate the total calories, also provide the details of every food items with calories intake
-               is below format
+and calculate the total calories, also provide the details of every food items with calories intake
+in the following format:
 
-               1. Item 1 - no of calories
-               2. Item 2 - no of calories
-               ----
-               ----
+1. Item 1 - number of calories
+2. Item 2 - number of calories
+---
+---
 """
 
 ## If submit button is clicked
