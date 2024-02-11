3b91f5dc8586163156bdf0476378034b9bdefc08
Hardik Parmar <121241956+hardikjp7@users.noreply.github.com>
2024-02-11 20:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,71 @@
-### Health Management APP
-from dotenv import load_dotenv
-
-load_dotenv() ## load all the environment variables
-
 import streamlit as st
-import os
 import google.generativeai as genai
 from PIL import Image
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Set page configuration
+st.set_page_config(page_title="Gemini Health App")
 
-## Function to load Google Gemini Pro Vision API And get response
+# Sidebar guide
+st.sidebar.markdown("""
+### Guide
+1. Enter your Gemini API key in the provided input field.
+2. The default prompt is already integrated into the application.
+3. Simply upload a photo of food to receive calorie information.
+4. For additional details or a custom prompt, utilize the "Provide Prompt" input to access extra information via the Gemini Vision Pro model.
+""")
 
-def get_gemini_repsonse(input,image,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
-    response=model.generate_content([input,image[0],prompt])
+# Configure Google Gemini Pro Vision API with the API key from the input field
+api_key = st.sidebar.text_input("Enter your Google API Key:", key="api_key")
+if api_key:
+    genai.configure(api_key=api_key)
+else:
+    st.warning("Please enter your Google API Key.")
+
+def get_gemini_response(input, image, prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input, image[0], prompt])
     return response.text
 
 def input_image_setup(uploaded_file):
-    # Check if a file has been uploaded
     if uploaded_file is not None:
-        # Read the file into bytes
         bytes_data = uploaded_file.getvalue()
 
         image_parts = [
             {
-                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "mime_type": uploaded_file.type,
                 "data": bytes_data
             }
         ]
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
-    
-##initialize our streamlit app
 
-st.set_page_config(page_title="Gemini Health App")
+# Initialize our Streamlit app
+st.title("Gemini Health App")
 
-st.header("Gemini Health App")
-input=st.text_input("Input Prompt: ",key="input")
+input_text = st.text_input("Input Prompt: ", key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-image=""   
+image = ""
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
+submit = st.button("Tell me the total calories")
 
-submit=st.button("Tell me the total calories")
+input_prompt = """
+You are an expert in nutrition where you need to see the food items from the image
+and calculate the total calories, also provide the details of every food items with calories intake
+in the following format:
 
-input_prompt="""
-You are an expert in nutritionist where you need to see the food items from the image
-               and calculate the total calories, also provide the details of every food items with calories intake
-               is below format
-
-               1. Item 1 - no of calories
-               2. Item 2 - no of calories
-               ----
-               ----
-
-
+1. Item 1 - number of calories
+2. Item 2 - number of calories
+---
+---
 """
 
-## If submit button is clicked
-
 if submit:
-    image_data=input_image_setup(uploaded_file)
-    response=get_gemini_repsonse(input_prompt,image_data,input)
+    image_data = input_image_setup(uploaded_file)
+    response = get_gemini_response(input_prompt, image_data, input_text)
     st.subheader("The Response is")
     st.write(response)
-
-
